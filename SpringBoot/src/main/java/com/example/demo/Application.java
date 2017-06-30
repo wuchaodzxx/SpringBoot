03059594064039d696319e873492d583803a8f32
wuchaodzxx <wuchaodzxx@126.com>
2017-06-30 22:37:34 +0800

    dfsdf

--- SpringBoot/src/main/java/com/example/demo/Application.java
+++ SpringBoot/src/main/java/com/example/demo/Application.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
